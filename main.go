0d6c742912a5c21d8fe83ca72162549d4fac42f9
Marco Grossmann <marco.grossmann@meteoviva.com>
2024-03-14 22:35:22 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+
+func add(x, y int) int {
+	return x + y
+}
+
+
+func swap(x, y string) (string, string) {
+	return y,x
+}
+
+func main() {
+	fmt.Println(add(23, 23))
+}
